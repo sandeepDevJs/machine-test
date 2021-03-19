32f99c1fdec3f12b95b680ba9390eff170a955fd
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-03-19 12:36:38 +0530

    nav

--- src/components/NavBar.component.jsx
+++ src/components/NavBar.component.jsx
@@ -5,7 +5,7 @@ import { LinkContainer } from "react-router-bootstrap";
 const NavBar = () => {
 	return (
 		<>
-			<Navbar bg="secondary" variant="secondary">
+			<Navbar bg="secondary">
 				<LinkContainer className="navbar-brand" to="/">
 					<Navbar.Brand>LOGO</Navbar.Brand>
 				</LinkContainer>
