22b61e0322d69e64c6ba83b7a14e9e1fa39419d7
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-03-19 12:28:33 +0530

    homepage

--- src/components/HomePage/DropDown.component.jsx
+++ src/components/HomePage/DropDown.component.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Dropdown } from "react-bootstrap";
 
-const DropDown = () => {
+const DropDown = ({ data, settext }) => {
+	const onClickHandler = (text) => {
+		settext(text);
+	};
+
 	return (
 		<Dropdown>
 			<Dropdown.Toggle variant="success" id="dropdown-basic">
@@ -9,9 +13,12 @@ const DropDown = () => {
 			</Dropdown.Toggle>
 
 			<Dropdown.Menu>
-				<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-				<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-				<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+				{data.length &&
+					data.map((posts) => (
+						<Dropdown.Item onClick={() => onClickHandler(posts.body)}>
+							{posts.title}
+						</Dropdown.Item>
+					))}
 			</Dropdown.Menu>
 		</Dropdown>
 	);
--- src/components/HomePage/HomePage.component.jsx
+++ src/components/HomePage/HomePage.component.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Info from "./Info.component";
 import DropDown from "./DropDown.component";
+import Loader from "../Loader.component";
+import { Alert } from "react-bootstrap";
 
 const HomePage = () => {
+	const [data, setdata] = useState([]);
+	const [text, settext] = useState("");
+	const [err, seterr] = useState("");
+	const [loading, setloading] = useState(true);
+
+	useEffect(() => {
+		axios
+			.get("http://jsonplaceholder.typicode.com/posts")
+			.then((res) => {
+				setdata(res.data.slice(0, 4));
+				setloading(false);
+			})
+			.catch((err) => {
+				err.response && err.response.data.message
+					? seterr(err.response.data.message)
+					: seterr(err.message);
+
+				setloading(false);
+			});
+	}, []);
+
 	return (
 		<div className="container">
 			<br />
+			{loading && <Loader />}
+			{err && <Alert variant="danger">{err}</Alert>}
 			<br />
-			<DropDown />
+			<DropDown data={data} settext={settext} />
 			<br />
 			<br />
 			<br />
-			<Info />
+			<Info text={text ? text : "Select From DropDown To Display Text"} />
 		</div>
 	);
 };
